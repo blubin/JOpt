2b08461b2e1c3dbad67d3b4bdb289e15d9e5f62c
Fabio <islerfab@gmail.com>
2017-10-27 19:18:27 +0200

    Fixed merging issue

---
+++ src/test/java/edu/harvard/econcs/jopt/LPSolveTest.java
@@ -0,0 +1,58 @@
+package edu.harvard.econcs.jopt;
+
+import edu.harvard.econcs.jopt.solver.IMIP;
+import edu.harvard.econcs.jopt.solver.IMIPResult;
+import edu.harvard.econcs.jopt.solver.MIPException;
+import edu.harvard.econcs.jopt.solver.SolveParam;
+import edu.harvard.econcs.jopt.solver.client.SolverClient;
+import edu.harvard.econcs.jopt.solver.server.cplex.CPlexMIPSolver;
+import edu.harvard.econcs.jopt.solver.server.lpsolve.LPSolveMIPSolver;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * @author Fabio Isler
+ */
+public class LPSolveTest {
+
+    @Test
+    public void compareResultsToCplex() {
+        IMIP mip = TestSuite.provideBanalExample();
+
+        SolverClient lpSolveSolverClient = new SolverClient(new LPSolveMIPSolver());
+        IMIPResult lpSolveResult = lpSolveSolverClient.solve(mip);
+        SolverClient cplexSolverClient = new SolverClient(new CPlexMIPSolver());
+        IMIPResult cplexResult = cplexSolverClient.solve(mip);
+        TestSuite.compareMultipleResults(lpSolveResult, cplexResult);
+    }
+
+    @Test
+    public void testTimeoutBehavior() {
+        IMIP mip = TestSuite.provideComplexExample();
+        mip.setSolveParam(SolveParam.TIME_LIMIT, 2.0);
+        SolverClient lpSolveSolverClient = new SolverClient(new LPSolveMIPSolver());
+
+        try {
+            lpSolveSolverClient.solve(mip);
+        } catch (MIPException e) {
+            fail(e.getMessage());
+        }
+
+        mip.setSolveParam(SolveParam.ACCEPT_SUBOPTIMAL, true);
+        try {
+            lpSolveSolverClient.solve(mip);
+        } catch (MIPException e) {
+            fail(e.getMessage());
+        }
+
+        mip.setSolveParam(SolveParam.ACCEPT_SUBOPTIMAL, false);
+        try {
+            lpSolveSolverClient.solve(mip);
+            fail("Should have failed. Was it too fast?");
+        } catch (MIPException e) {
+            // Success - should throw this error
+            System.out.println("Successfully caught exception for the timeout.");
+        }
+    }
+}
