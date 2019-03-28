1fa8b443ec2413806949203e4be86ae9ae4f7f77
fabio <islerfab@gmail.com>
2019-03-28 15:49:24 +0400

    fix(sol-pool): reverted last change, because it may have side effects on certain runs

--- src/main/java/edu/harvard/econcs/jopt/solver/server/cplex/CPlexMIPSolver.java
+++ src/main/java/edu/harvard/econcs/jopt/solver/server/cplex/CPlexMIPSolver.java
@@ -302,36 +302,6 @@ public class CPlexMIPSolver implements IMIPSolver {
                         cplex.populate();
                         IloCplex.CplexStatus status = cplex.getCplexStatus();
                         logger.debug("Initial Status: {}", status);
-                        double absSolPoolGap = 1e75;
-                        double relSolPoolGap = 1e75;
-
-                        /*
-                         * We already set an absolute gap here to reduce the effect of a certain bug in CPLEX:
-                         *
-                         * There is a bug in CPLEX when it comes to counting the number of replaced
-                         *    solutions in the pool (solutions that at some point were in the pool
-                         *    but then got replaced by better solutions). This bug shows only when
-                         *    calling populate() repeatedly. The effect of this bug is what you
-                         *    observed in iterations 2-25: we essentially call populate() with a
-                         *    solution limit of 0! Eventually, the wrong counting of replaced solutions
-                         *    yields a negative number and at that point it is as if we called
-                         *    populate() with an infinite limit (this is the last iteration that then
-                         *    finds all the remaining solutions).
-                         *
-                         * This algorithm should generally avoid it, however, there have been cases where it occurred
-                         * if the initial number of solutions is very low (due to many duplicates). By setting the
-                         * absolute gap already here, we at least reduce the amount of solutions CPLEX tries to find in
-                         * the very last round (which otherwise can lead to a incredibly long duplicate elimination).
-                         */
-                        if (cplex.getSolnPoolNsolns() >= finalSolutionPoolCapacity) {
-                            absSolPoolGap = getAbsoluteSolutionPoolGap(cplex);
-                            relSolPoolGap = absSolPoolGap / (1e-10 + Math.abs(cplex.getObjValue()));
-                            logger.debug("Setting the initial absolute solution pool gap to {}.", absSolPoolGap);
-                            cplex.setParam(DoubleParam.SolnPoolAGap, absSolPoolGap);
-                        } else {
-                            logger.info("The initial populate() in solution pool mode 4 call did not provide many" +
-                                    "solutions, which indicates that there aren't many solutions available in general.");
-                        }
 
                         double absoluteSolutionPoolGapTolerance = mip.getDoubleSolveParam(SolveParam.SOLUTION_POOL_MODE_4_ABSOLUTE_GAP_TOLERANCE, 0);
                         double relativeSolutionPoolGapTolerance = mip.getDoubleSolveParam(SolveParam.SOLUTION_POOL_MODE_4_RELATIVE_GAP_TOLERANCE, 0);
@@ -341,7 +311,8 @@ public class CPlexMIPSolver implements IMIPSolver {
                                     "that case, you must rely on the absolute gap tolerance.");
                         }
                         int count = 0;
-
+                        double absSolPoolGap = 1e75;
+                        double relSolPoolGap = 1e75;
                         long startTimeOfSolutionPool = System.currentTimeMillis();
                         while (IloCplex.CplexStatus.PopulateSolLim.equals(status) || IloCplex.CplexStatus.AbortTimeLim.equals(status)) {
 
